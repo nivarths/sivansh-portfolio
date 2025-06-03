ae6f708c9a8ae51386f90008b576a98d8728848e
nivarths <nivarths@mcmaster.ca>
2025-06-02 20:14:31 -0400

    formatting changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import logo from '../assets/portfolio-logo-2.svg'
 
 export const Navbar = ({menuOpen, setMenuOpen}) => {
     useEffect(() => {
@@ -9,9 +10,11 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white">
-                        Sivansh <span className="text-blue-500">Nivarthi</span>
-                        </a>
+                    <a href="#home" className="flex items-center space-x-2 font-mono text-xl font-bold text-white">
+                        <span>
+                            Sivansh <span className="text-blue-500">Nivarthi</span>
+                        </span>
+                    </a>
 
                     {/* mobile menu */}
                     {!menuOpen && (
